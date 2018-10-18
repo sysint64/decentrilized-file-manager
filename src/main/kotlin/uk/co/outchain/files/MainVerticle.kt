9856401b65057f614b9db0014db034dc6958afb1
Andrey Kabylin <andrey@kabylin.ru>
2018-10-18 15:55:33 +0700

    kotlin config

--- src/main/kotlin/uk/co/outchain/files/MainVerticle.kt
+++ src/main/kotlin/uk/co/outchain/files/MainVerticle.kt
@@ -1,7 +1,36 @@
 package uk.co.outchain.files
 
-import io.vertx.core.AbstractVerticle
+import io.reactivex.rxkotlin.subscribeBy
+import io.vertx.core.DeploymentOptions
+import io.vertx.core.Future
+import io.vertx.reactivex.core.AbstractVerticle
+import io.vertx.reactivex.config.ConfigRetriever
+import org.slf4j.LoggerFactory
 
 class MainVerticle : AbstractVerticle() {
+    companion object {
+        private val LOGGER = LoggerFactory.getLogger(MainVerticle::class.java)
+    }
 
+    override fun start(startFuture: Future<Void>) {
+        val retriever = ConfigRetriever.create(vertx)
+
+        retriever.rxGetConfig()
+            .flatMap { config ->
+                vertx
+                    .rxDeployVerticle(
+                        "uk.co.outchain.files.client.ClientVerticle",
+                        DeploymentOptions()
+                            .setConfig(config)
+                            .setInstances(1)
+                    )
+            }
+            .subscribeBy(
+                onSuccess = {
+                    startFuture.complete()
+                    LOGGER.info("Successfully started")
+                },
+                onError = startFuture::fail
+            )
+    }
 }
---
+++ src/main/kotlin/uk/co/outchain/files/client/ClientVerticle.kt
@@ -0,0 +1,7 @@
+package uk.co.outchain.files.client
+
+import io.vertx.reactivex.core.AbstractVerticle
+
+class ClientVerticle : AbstractVerticle() {
+
+}
